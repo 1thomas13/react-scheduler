6dcc7c61d4d22cb32a1c1abdb73ee20056777731
Dante Bonino <dantebonino3@gmail.com>
2024-02-08 16:13:40 +0000

    Reverted undesirable changes

--- src/components/LeftColumn/LeftColumnItem/LeftColumnItem.tsx
+++ src/components/LeftColumn/LeftColumnItem/LeftColumnItem.tsx
@@ -10,13 +10,13 @@ import {
 } from "./styles";
 import { LeftColumnItemProps } from "./types";
 
-const LeftColumnItem: FC<LeftColumnItemProps> = ({ id, item, startDate, rows, onItemClick }) => {
+const LeftColumnItem: FC<LeftColumnItemProps> = ({ id, item, rows, onItemClick }) => {
   return (
     <StyledWrapper
       title={item.title + " | " + item.subtitle}
       clickable={typeof onItemClick === "function"}
       rows={rows}
-      onClick={() => onItemClick?.({ id, label: item, startDate })}>
+      onClick={() => onItemClick?.({ id, label: item })}>
       <StyledInnerWrapper>
         <StyledImageWrapper>
           {item.icon ? (
